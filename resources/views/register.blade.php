357f48ebbc7b5756bd55a2c3c1381b2978a66d6c
Akiraff2015 <akiraffmc@gmail.com>
2016-12-10 08:18:46 +0800

    First commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,8 @@
+@extends('template')
+
+@section('title', "Registration Form")
+
+@section('content')
+  <p>Hello there</p>
+  {{$path}}
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register', function() {
+  $currentPath = Route::getFacadeRoot()->current()->uri();
+  return view('register')->
+    with('path', $currentPath);
+});
